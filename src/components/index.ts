69f1083564c85a3983af148696caa34237741741
Max Quiet <qbite0@gmail.com>
2022-04-24 23:21:26 +0700

    update components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { default as Button } from "./Button";
+export { default as CSSReset } from "./CSSReset";
+export { default as Dropdown } from "./Dropdown";
 export { default as Input } from "./Input";
 export { default as Progress } from "./Progress";
+export { default as Provider } from "./Provider";
